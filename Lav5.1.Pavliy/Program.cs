f1d1341bdd95f60d4249f8515c6374c15b2d3c6f
Asus <Asus@DESKTOP-1IKSUBO>
2023-11-15 17:35:25 +0200

    Добавьте файлы проекта.

---
+++ Lav5.1.Pavliy/Program.cs
@@ -0,0 +1,34 @@
+﻿//5.1
+//char[] symbols = { 'a', 'b', 'c', '(', 'x', 'y', 'z', ')', 'd', 'e', 'f' };
+
+//int openingIndex = -1;
+//int c = -1;
+//for (int i = 0; i < symbols.Length; i++)
+//{
+//    if (symbols[i] == '(')
+//    {
+//        openingIndex = i;
+//    }
+//    else if (symbols[i] == ')')
+//    {
+//        c = i;
+//        break;
+//    }
+//}
+//if (openingIndex != -1 && c != -1)
+//{
+//    for (int i = openingIndex + 1; i < c; i++)
+//    {
+//        Console.Write(symbols[i]);
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Открывающаяся или закрывающаяся скобка не найдена.");
+//}
+
+
+//5.2 слож
+string st = "Дано предложение. Напечатать все его слова, отличные от слова привет пчел!!!";
+Console.WriteLine(string.Join(" ", st.Split().Where(i => i != "привет")));
+
